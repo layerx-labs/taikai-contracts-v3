33676b4750f8f72d543c0aa296da0079556abf4d
Helder Vasconcelos <heldervasc@bearstouch.com>
2022-12-12 10:09:38 +0000

    Token is paused

--- contracts/TCertificate.sol
+++ contracts/TCertificate.sol
@@ -4,17 +4,19 @@ pragma abicoder v2;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-
+import "@openzeppelin/contracts/utils/Counters.sol";
 /*
  */
-contract TAIKAICertificate is ERC721URIStorage, Ownable {
-
-    uint256 private _nextMintId = 0 ;
+contract TAIKAICertificate is ERC721URIStorage, Ownable, Pausable {
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
 
     constructor(string memory name_, string memory symbol_, address owner) 
         ERC721URIStorage() 
         ERC721(name_, symbol_)
         Ownable() 
+        Pausable()  
     {
         transferOwnership(owner);        
     }
@@ -25,18 +27,27 @@ contract TAIKAICertificate is ERC721URIStorage, Ownable {
      * Assert rules,
      *  msg.sender must be dispatcher
      */
-    function mint(address to, string memory uri ) external onlyOwner  {
-        uint256 id = _nextMintId;
+    function mint(address to, string memory uri ) public virtual onlyOwner whenNotPaused {
+        uint256 id = _tokenIdTracker.current();
         _safeMint(to, id);
         _setTokenURI(id, uri);
-        _nextMintId++;
+        _tokenIdTracker.increment();
     }
 
     function getNextId() external view returns (uint256) {
-        return _nextMintId;
+        return _tokenIdTracker.current();
+    }
+
+    function pause() external virtual onlyOwner {    
+        _pause();
+    }
+
+    function unpause() external virtual onlyOwner {
+        _unpause();
     }
 
     /** Certificate is not transferable */
     function transferFrom(address from, address to, uint256 tokenId) public override { revert(); }
     function safeTransferFrom(address from, address to, uint256 tokenId) public override { revert(); }
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override { revert(); }
 }
