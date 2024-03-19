25fb77ed8291a3de5b53e7cbc6f43cf15e510f6e
JOAO RICARDO CAVICHIOLLI <joao.cavichiolli@gmail.com>
2024-03-19 08:26:53 -0300

    Update networks.ts

--- config/networks.ts
+++ config/networks.ts
@@ -3,7 +3,7 @@ import { STAGING_ACCOUNTS_PKEYS} from "./constants";
 export const STAGING_NETWORKS = {
      vp: {
         chainId: 1600,
-        url: "https://eth-vp.taikai.network:18080",
+        url: "https://eth-vp.taikai.network",
         accounts: STAGING_ACCOUNTS_PKEYS
       },
       eden: {
@@ -13,22 +13,22 @@ export const STAGING_NETWORKS = {
       },
       gaia: {
         chainId: 1602,
-        url: "https://eth-gaia.taikai.network:18080",
+        url: "https://eth-gaia.taikai.network",
         accounts: STAGING_ACCOUNTS_PKEYS
       },
       eva: {
         chainId: 1603,
-        url: "https://eth-eva.taikai.network:18080",
+        url: "https://eth-eva.taikai.network",
         accounts: STAGING_ACCOUNTS_PKEYS
       },
       atena: {
         chainId: 1604,
-        url: "https://eth-atena.taikai.network:18080",
+        url: "https://eth-atena.taikai.network",
         accounts: STAGING_ACCOUNTS_PKEYS 
       },
       heras: {
         chainId: 1605,
-        url: "https://eth-heras.taikai.network:18080",
+        url: "https://eth-heras.taikai.network",
         accounts: STAGING_ACCOUNTS_PKEYS 
       },
 }
